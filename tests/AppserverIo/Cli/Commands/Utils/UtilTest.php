890af4e8c4ae00fa4730c59c0127a01e38f6dc41
Alexandros Weigl <a.weigl@techdivision.com>
2016-09-13 18:23:36 +0200

    Added basic test for Util

---
+++ tests/AppserverIo/Cli/Commands/Utils/UtilTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ *
+ *
+ * @author    Alexandros Weigl <a.weigl@techdivision.com>
+ * @copyright 2016 TechDivision GmbH <info@appserver.io>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link
+ */
+
+
+namespace AppserverIo\Cli\Commands\Utils;
+
+
+class UtilTest extends \PHPUnit_Framework_TestCase
+{
+    const WEB = 'web.xml';
+    protected $fileName;
+    protected $template;
+    protected $directory;
+    protected $route;
+    protected $applicationName;
+    protected $namespace;
+
+
+    public function setUp() {
+        $this->fileName = self::WEB;
+        $this->template = __DIR__ . '/../../../../../templates/static/WEB-INF/web.xml';
+        echo $this->template;
+        $this->template = realpath($this->template);
+        $this->directory = __DIR__ . '/';
+        $this->route = 'false';
+        $this->applicationName = 'testunit';
+        $this->namespace = 'testing\\test';
+
+    }
+
+    public function testPutFile() {
+        echo __DIR__;
+        Util::putFile($this->fileName, $this->template, $this->directory, $this->route, $this->applicationName, $this->namespace);
+        $this->assertTrue(file_exists($this->directory . self::WEB));
+    }
+
+    public function tearDown() {
+        unlink(__DIR__ . '/web.xml');
+    }
+}
