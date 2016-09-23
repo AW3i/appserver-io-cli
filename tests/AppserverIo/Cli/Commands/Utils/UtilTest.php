fc0ed84bce7d9855c5e9d72b47cfb9d197396575
Alexandros Weigl <a.weigl@techdivision.com>
2016-09-23 14:46:46 +0200

    Refactored UtilTest

--- tests/AppserverIo/Cli/Commands/Utils/UtilTest.php
+++ tests/AppserverIo/Cli/Commands/Utils/UtilTest.php
@@ -5,6 +5,7 @@ namespace AppserverIo\Cli\Commands\Utils;
 use AppserverIo\Cli\Commands\Utils\DirKeys;
 use AppserverIo\Properties\Properties;
 use AppserverIo\Cli\Commands\Utils\FilesystemUtil;
+use org\bovigo\vfs\vfsStream;
 
 /**
  * Tests Util
@@ -17,74 +18,31 @@ use AppserverIo\Cli\Commands\Utils\FilesystemUtil;
 class UtilTest extends \PHPUnit_Framework_TestCase
 {
     const WEB = 'web.xml';
-    protected $fileName;
     protected $template;
-    protected $properties;
+    protected $namespace;
     protected $dirNamespace;
-    protected $webInf;
-    protected $common;
-    protected $dhtml;
-    private $metaInf;
-
 
     public function setUp()
     {
-        $this->properties = new Properties();
-        $this->properties->add('directory', __DIR__ . '/test');
-        $this->properties->add('path', 'index');
-        $this->properties->add('application-name', 'testunit');
-        $this->properties->add('namespace', 'testing\\test');
-        $this->dirNamespace = str_replace('\\', '/', $this->properties->getProperty('namespace'));
-        $this->dhtml = $this->properties->getProperty('directory') . DIRECTORY_SEPARATOR . DirKeys::DHTML;
-        $this->metaInf = $this->properties->getProperty('directory') . DIRECTORY_SEPARATOR . DirKeys::METACLASSES . $this->dirNamespace;
-        $this->webInf = $this->properties->getProperty('directory') . DIRECTORY_SEPARATOR . DirKeys::WEBCLASSES . $this->dirNamespace;
-        $this->common = $this->properties->getProperty('directory') . DIRECTORY_SEPARATOR . DirKeys::COMMONCLASSES . $this->dirNamespace;
+        $this->namespace = 'testing\\test';
+        $this->dirNamespace = str_replace('\\', '/', $this->namespace);
 
-        $this->fileName = self::WEB;
         $this->template = realpath(DirKeys::STATICTEMPLATES . DIRECTORY_SEPARATOR . DirKeys::WEBINF . DIRECTORY_SEPARATOR . self::WEB);
-    }
-
-    /**
-     * tests the create Directories method
-     *
-     * @return void
-     */
-    public function testCreateDirectoriesCreatesDirectories()
-    {
-        FilesystemUtil::createDirectories($this->properties->getProperty('directory'), $this->properties->getProperty('namespace'));
-        $this->assertTrue(file_exists($this->properties->getProperty('directory') . DIRECTORY_SEPARATOR . ('index.do')));
-        $this->assertTrue(is_dir($this->dhtml));
-        $this->assertTrue(is_dir($this->webInf));
-        $this->assertTrue(is_dir($this->metaInf));
-        $this->assertTrue(is_dir($this->common));
     }
 
     public function testSlashToBackSlashReturnsSlash()
     {
-
-        $this->assertEquals($this->properties->getProperty('namespace'), Util::slashToBackSlash($this->dirNamespace));
+        $this->assertEquals($this->namespace, Util::slashToBackSlash($this->dirNamespace));
     }
 
     public function testSlashToBackSlashReturnsVariableIfNoMatchIsFound() {
-        $this->assertEquals($this->properties->getProperty('namespace'), Util::slashToBackSlash($this->properties->getProperty('namespace')));
-    }
-
-    public function testValidateArgumentsReturnsTrue()
-    {
-        $this->assertTrue(Util::validateArguments($this->properties));
-    }
-
-    public function testValidateArgumentsThrowsInvalidArgumentException()
-    {
-        $this->properties->add('empty', null);
-        $this->expectException(\InvalidArgumentException::class);
-        Util::validateArguments($this->properties);
+        $this->assertEquals($this->namespace, Util::slashToBackSlash($this->namespace));
     }
 
     public function testBuildDynamicDirectoryReturnsValidPathOnCorrectInput()
     {
         $this->template = DirKeys::DYNAMICTEMPLATES . 'WEB-INF/classes/Utils/RequestKeys.php.template';
-        $path = Util::buildDynamicDirectory($this->template, $this->properties->getProperty('namespace'));
+        $path = Util::buildDynamicDirectory($this->template, $this->namespace);
         $expectedPath = DirKeys::WEBCLASSES . $this->dirNamespace . DIRECTORY_SEPARATOR . 'Utils' . DIRECTORY_SEPARATOR;
         $this->assertEquals($expectedPath, $path);
     }
@@ -94,18 +52,12 @@ class UtilTest extends \PHPUnit_Framework_TestCase
         $this->template = 'WEB-INF/classes/Utils/RequestKeys.php.template';
 
         $this->expectException(\InvalidArgumentException::class);
-        $path = Util::buildDynamicDirectory($this->template, $this->properties->getProperty('namespace'));
+        $path = Util::buildDynamicDirectory($this->template, $this->namespace);
     }
 
     public function testGetTemplateThrowsInvalidArgumentException()
     {
         $this->expectException(\InvalidArgumentException::class);
         $path = Util::getTemplate($this->template);
-
-    }
-
-    public function tearDown()
-    {
-        FilesystemUtil::deleteFiles($this->properties->getProperty('directory') . DIRECTORY_SEPARATOR);
     }
 }
